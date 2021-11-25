d21aac92c0ec562a381ab104badf6d890e4e2268
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2021-11-25 15:38:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,27 @@ Webcam.attach('#camera');
 
 function take_snapshot() {
     Webcam.snap(function (data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '">';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
     });
 }
-console.log('ml5 version: ', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tufu41o08/model.json', modelLoaded);
+console.log('ml5 version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fv5l9Up93/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded');
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3) * 100 + "%";
+    }
+}
